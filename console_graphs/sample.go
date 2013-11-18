80ac40114c4cc06f4faca15c812d03314b8c12d2
Fernando Ipar <fipar@acm.org>
2013-11-18 16:22:47 -0200

    added sample gauge in go

---
+++ console_graphs/sample.go
@@ -0,0 +1,80 @@
+package main
+
+/*
+This idea can be expanded into a reusable gauge so you can graphically represent some info like cpu load, used mem, etc while sharing console real estate with textual information
+*/
+
+import (
+	"github.com/tncardoso/gocurses"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+const (
+	GAUGE_MAX    = 5
+	GAUGE_HEIGHT = 7
+	GAUGE_WIDTH  = 3
+	ITEM         = 'X'
+	BLANK        = ' '
+)
+
+// eventually this one will do any needed transformation to fit a (potentially floating point) number into a gauge
+func normalize(num int) int {
+	if num > GAUGE_MAX {
+		return GAUGE_MAX
+	} else {
+		return num
+	}
+}
+
+// for now, gauges will have a fixed height
+func gauge(datasource chan int, rowstart int, colstart int) {
+	win := gocurses.NewWindow(GAUGE_HEIGHT, GAUGE_WIDTH, colstart, rowstart)
+	win.Box(0, 0)
+	win.Refresh()
+	for {
+		num := <-datasource
+		gocurses.Refresh()
+		num = normalize(num)
+		color_pair := 1
+		switch num {
+		default:
+			color_pair = 1
+		case 3, 4:
+			color_pair = 2
+		case 5:
+			color_pair = 3
+		}
+		gocurses.Mvaddstr(1, 0, strconv.Itoa(num))
+		for i := rowstart + GAUGE_HEIGHT - 2; i >= rowstart+GAUGE_HEIGHT-6; i-- {
+			if i > rowstart+GAUGE_MAX-num {
+				gocurses.Attron(gocurses.ColorPair(color_pair))
+				gocurses.Mvaddch(i, colstart+1, ITEM)
+				gocurses.Attroff(gocurses.ColorPair(color_pair))
+			} else {
+				gocurses.Mvaddch(i, colstart+1, BLANK)
+			}
+		}
+	}
+}
+
+func main() {
+	gocurses.Initscr()
+	defer gocurses.End()
+	gocurses.Attron(gocurses.A_BOLD)
+	gocurses.Addstr("Test gauge")
+	gocurses.StartColor()
+	gocurses.InitPair(1, gocurses.COLOR_GREEN, gocurses.COLOR_BLACK)
+	gocurses.InitPair(2, gocurses.COLOR_YELLOW, gocurses.COLOR_BLACK)
+	gocurses.InitPair(3, gocurses.COLOR_RED, gocurses.COLOR_BLACK)
+	gocurses.Refresh()
+
+	source := make(chan int)
+	go gauge(source, 10, 10)
+	r := rand.New(rand.NewSource(99))
+	for {
+		source <- r.Intn(7)
+		time.Sleep(1 * time.Second)
+	}
+}
