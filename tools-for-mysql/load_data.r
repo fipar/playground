4b58255c28433232622e42534fcbf9cdb6728f68
Fernando Ipar <fipar@acm.org>
2013-12-05 12:01:11 -0200

    turned load_data into actually executable R batch

--- tools-for-mysql/load_data.r
+++ tools-for-mysql/load_data.r
@@ -1,3 +1,9 @@
+# to run: 
+# R CMD BATCH ./load_data.r
+require(RMySQL)
+require(ggplot2)
 con <- dbConnect(MySQL(), user="msandbox", password="msandbox", dbname="r_input", host="127.0.0.1", port=5527)
 data_from_processlist <- dbGetQuery(con, "select ts,Command,count(pk) as cnt from processlist_captures group by ts,Command order by ts asc;")
+png("genplot.png",height=800,width=800)
 qplot(data=data_from_processlist, x=ts, y=cnt, color=Command)
+dev.off()
