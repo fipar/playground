225aada35073e87d345e5e8f40a3a6cf32fb9d17
Fernando Ipar <fipar@acm.org>
2014-01-01 19:23:48 -0200

    added skel for clojure main app

---
+++ clj_sample/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj_sample
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clj_sample/test/clj_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-sample.core-test
+  (:use clojure.test
+        clj-sample.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
