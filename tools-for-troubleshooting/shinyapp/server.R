21502cc4ed7a16f3dca0a37976c6b298d90ed8e2
Fernando Ipar <fipar@acm.org>
2015-09-19 00:33:55 -0300

    added reference

--- tools-for-troubleshooting/shinyapp/server.R
+++ tools-for-troubleshooting/shinyapp/server.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(ggplot2)
 
-# Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
     ranges <- reactiveValues(x = NULL, y = NULL)
 
@@ -9,11 +8,11 @@ shinyServer(function(input, output) {
     environment <- environment()
     ggplot(mongostat[], aes(x=mongostat[,17],y=mongostat[,2])) + geom_point() 
     output$mongostatPlot <- renderPlot({
-       # plot(mongostat[,17], mongostat[,2])
         ggplot(mongostat[], aes(x=mongostat[,17],y=mongostat[,2]), environment = environment) + geom_point() + coord_cartesian(xlim=ranges$x, ylim=ranges$y)
     })
     # When a double-click happens, check if there's a brush on the plot.
     # If so, zoom to the brush bounds; if not, reset the zoom.
+    # Taken from http://shiny.rstudio.com/gallery/plot-interaction-zoom.html
     observeEvent(input$mongostat_dblclick, {
       brush <- input$mongostat_brush
       if (!is.null(brush)) {
