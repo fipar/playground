d6bca50e159e6216a690f5fd67b32e5ab2df9730
Fernando Ipar <fipar@acm.org>
2015-09-23 01:11:33 -0300

    added timestamp to captures

--- tools-for-troubleshooting/data-capture/collect-mysqladmin.sh
+++ tools-for-troubleshooting/data-capture/collect-mysqladmin.sh
@@ -13,9 +13,9 @@ duration=$2
 dest=${TOOLNAME}_$(ts).gz
 trap "rm -f $dest.pid" SIGINT SIGTERM SIGHUP
 # get the header.
-$TOOLNAME ext -i 1 -c 1|grep -v '-'|awk -F '|' '{print $2}'|grep -v ^$|grep -v 'Variable_name' |tr '\n' ' '|sed 's/  */ /g'|sed 's/^ //g' > $dest.header
+$TOOLNAME ext -i 1 -c 1|grep -v '-'|awk -F '|' '{print $2}'|grep -v ^$|sed 's/Variable_name/Timestamp/g'|tr '\n' ' '|sed 's/  */ /g'|sed 's/^ //g' > $dest.header
 # now get the capture 
-$TOOLNAME ext -i $interval -c $duration | awk -F '|' '{print $3}'|sed 's/^ //g'|sed 's/  *$/ /g'|grep -v ^$|sed 's/^ $/_/g' |tr '\n' ' '| sed 's/Value/|/g' | tr '|' '\n'| grep -v ^$|sed 's/^  *//g' |sed 's/  */ /g'| gzip -c > $dest &
+$TOOLNAME ext -i $interval -c $duration | awk -F '|' '{print $3}'|sed 's/^ //g'|sed 's/  *$/ /g'|grep -v ^$|sed 's/^ $/_/g' |tr '\n' ' '| sed "s/Value/|`date '+%Y-%m-%d_%H:%M:%S'`/g" | tr '|' '\n'| grep -v ^$|sed 's/^  *//g' |sed 's/  */ /g'| gzip -c > $dest &
 echo $! > $dest.pid
 pid=$!
 # save the pid so we can monitor disk space while the tool runs, and
